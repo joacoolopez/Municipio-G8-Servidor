8e58c6ac0b6109d4c7fcb4165b8ab91acb2940dd
joaco <lopezjoaco02@gmail.com>
2024-05-19 17:03:18 -0300

    puerto

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import userVecinoRoute from "./routes/userVecino.routes.js"
 import serviciosRoute from "./routes/servicios.routes.js"
+const PORT = process.env.PORT || 4000;
 
 const app = express()
 app.use(express.json())
@@ -9,5 +10,5 @@ app.use(express.json())
 app.use("/api/userVecino", userVecinoRoute)
 app.use("/api/servicios", serviciosRoute)
 
-app.listen(3000);
-console.log("Server on port", 3000);
+app.listen(PORT);
+console.log("Server on port", PORT);
