d43a5430b13aecd6f4f55808ecc688267ea02df0
Vicky <vickyendendyk@hotmail.com>
2024-06-09 16:46:37 -0300

    arreglo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import '../config.js'; //Para que tome las variables de entorno del .env si no no se estaria conectand a la bd
 
-import comerciosRoute from "./routes/comercios.routes.js";
+//import comerciosRoute from "./routes/comercios.routes.js";
 import reclamosRoute from "./routes/reclamos.routes.js";
 import serviciosRoute from "./routes/servicios.routes.js";
 import userVecinoRoute from "./routes/userVecino.routes.js";
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use("/api/userVecino", userVecinoRoute)
 app.use("/api/servicios", serviciosRoute)
 app.use("/api/reclamos", reclamosRoute)
-app.use("/api/comercios", comerciosRoute)//
+//app.use("/api/comercios", comerciosRoute)
 
 app.listen(PORT);
 console.log("Server on port", PORT);
