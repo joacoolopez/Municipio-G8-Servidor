bfcdf35c0e1b519011a5112b5ea46facc9f818ac
Vicky <vickyendendyk@hotmail.com>
2024-06-09 15:08:07 -0300

    Modificaciones

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import '../config.js'; //Para que tome las variables de entorno del .env si no no se estaria conectand a la bd
 import express from "express";
+import '../config.js'; //Para que tome las variables de entorno del .env si no no se estaria conectand a la bd
 
-import userVecinoRoute from "./routes/userVecino.routes.js"
-import serviciosRoute from "./routes/servicios.routes.js"
-import reclamosRoute from "./routes/reclamos.routes.js"
+import comerciosRoute from "./routes/comercios.routes.js";
+import reclamosRoute from "./routes/reclamos.routes.js";
+import serviciosRoute from "./routes/servicios.routes.js";
+import userVecinoRoute from "./routes/userVecino.routes.js";
 
 const PORT = process.env.PORT || 4000;
 
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use("/api/userVecino", userVecinoRoute)
 app.use("/api/servicios", serviciosRoute)
 app.use("/api/reclamos", reclamosRoute)
+app.use("/api/comercios", comerciosRoute)
 
 app.listen(PORT);
 console.log("Server on port", PORT);
